34a7786383456498f957e547eba98ef4077dd190
ucynthia13 <cynthiaumwali3@gmail.com>
2026-07-25 22:43:24 +0200

    feat(validation): add borrowing limit and validation checks to manual_check

--- manual_check.py
+++ manual_check.py
@@ -1,4 +1,6 @@
 
+from validation.run_validation import run_validation
+from validation.borrowing_limit_check import check_borrowing_limit
 from validation.eligibility_check import check_eligibility
 from validation.collateral_admissibility_check import (
     check_collateral_admissibility,
@@ -14,3 +16,5 @@ if __name__ == "__main__":
     print(check_eligibility(TEST_BANK_ID))
     print(check_collateral_admissibility(TEST_REQUEST_ID))
     print(check_collateral_value(TEST_REQUEST_ID))
+    print(check_borrowing_limit(TEST_REQUEST_ID))
+    print(run_validation(TEST_REQUEST_ID))

5d1fa18d6fc08768ff0b1a57e1f52e3ad82a5577
Isimbi Nina <h.isimbi@alustudent.com>
2026-07-25 12:19:01 +0200

    Added foreign keys and enums

--- db_schema.sql
+++ db_schema.sql
@@ -6,29 +6,29 @@ USE central_bank_system;
 CREATE TABLE central_bank (
 	central_bank_id VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE,
 	password_hash VARCHAR(250) NOT NULL,
-	created_at DATETIME NOT NULL
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE commercial_banks (
 	bank_id INT PRIMARY KEY AUTO_INCREMENT,
 	bank_name VARCHAR(250) NOT NULL,
 	password_hash VARCHAR(250) NOT NULL,
-	eligibility VARCHAR(30) NOT NULL,
-	borrowing_limit DECIMAL,
-	created_at DATETIME NOT NULL
+	eligibility ENUM('none','eligible') NOT NULL,
+	borrowing_limit DECIMAL(15,2) NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE collateral_types (
 	collateral_type_id INT PRIMARY KEY AUTO_INCREMENT,
 	type_name VARCHAR(250) NOT NULL,
-	haircut_percentage DECIMAL NOT NULL
+	haircut_percentage DECIMAL(5,2) NOT NULL
 );
 
 CREATE TABLE collateral_inventory (
 	inventory_id INT PRIMARY KEY AUTO_INCREMENT,
 	bank_id INT NOT NULL,
 	collateral_type_id INT NOT NULL,
-	declared_value DECIMAL NOT NULL,
+	declared_value DECIMAL(15,2) NOT NULL,
 	FOREIGN KEY(bank_id) REFERENCES commercial_banks(bank_id),
 	FOREIGN KEY(collateral_type_id) REFERENCES collateral_types(collateral_type_id)
 );
@@ -37,20 +37,23 @@ CREATE TABLE weekly_operations (
 	operation_id INT PRIMARY KEY AUTO_INCREMENT,
 	start_date DATE NOT NULL,
 	end_date DATE NOT NULL,
-	status VARCHAR(50) NOT NULL
+	status ENUM('Open','Closed') NOT NULL
 );
 
 CREATE TABLE requests (
 	request_id INT PRIMARY KEY AUTO_INCREMENT,
 	bank_id INT NOT NULL,
 	operation_id INT NOT NULL,
-	requested_amount DECIMAL NOT NULL,
-	status VARCHAR(50) NOT NULL
+	requested_amount DECIMAL(15,2) NOT NULL,
+	status ENUM('Pending','Successful','Failed') NOT NULL,
+	FOREIGN KEY(bank_id) REFERENCES commercial_banks(bank_id),
+	FOREIGN KEY(operation_id) REFERENCES weekly_operations(operation_id)
 );
 
 CREATE TABLE request_collateral (
 	request_id INT NOT NULL,
 	inventory_id INT NOT NULL,
+    PRIMARY KEY(request_id, inventory_id),
 	FOREIGN KEY(inventory_id) REFERENCES collateral_inventory(inventory_id),
 	FOREIGN KEY(request_id) REFERENCES requests(request_id)
 );
@@ -66,8 +69,8 @@ CREATE TABLE rejections (
 CREATE TABLE allotments (
 	allotment_id INT PRIMARY KEY AUTO_INCREMENT,
 	request_id INT NOT NULL,
-	approved_amount DECIMAL NOT NULL,
-	policy_rate DECIMAL NOT NULL,
+	approved_amount DECIMAL(15,2) NOT NULL,
+	policy_rate DECIMAL(5,2) NOT NULL,
 	FOREIGN KEY(request_id) REFERENCES requests(request_id)
 );
 
@@ -76,6 +79,6 @@ CREATE TABLE settlements (
 	allotment_id INT NOT NULL,
 	settlement_date DATETIME NOT NULL,
 	repayment_date DATETIME NOT NULL,
-	interest_amount DECIMAL NOT NULL,
+	interest_amount DECIMAL(15,2) NOT NULL,
 	FOREIGN KEY(allotment_id) REFERENCES allotments(allotment_id)
 );
